8106b31416b0e31638d629d56bbe0aec0fec9cd8
Chung, Insang <insang@hansung.ac.kr>
2016-08-08 21:37:24 +0900

    Update and Add new GraderTest class

--- gitgrader/src/test/java/hansung/insang/gitgrader/GraderTest.java
+++ gitgrader/src/test/java/hansung/insang/gitgrader/GraderTest.java
@@ -5,9 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GraderTest {
+	
 	@Test
-	public void test_calculate_grade() {
+	public void test_calculate_grade1() {
 		Grade g = Grader.calculate_grade(30,30);
 		assertEquals(Grade.C, g);
 	}
+	@Test
+	public void test_calculate_grade2() {
+		Grade g = Grader.calculate_grade(40,40);
+		assertEquals(Grade.A, g);
+	}
+
 }
